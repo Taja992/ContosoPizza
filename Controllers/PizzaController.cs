57f461261b8ccad398bc23c50dc6c1f742972000
Taja992 <Tharaxui@gmail.com>
2024-08-27 11:54:58 +0200

    Initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,38 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+        PizzaServices.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaServices.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+
+    // GET by Id action
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
