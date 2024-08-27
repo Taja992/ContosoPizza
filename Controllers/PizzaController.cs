545b359d91d4f537c8d126c07557b08e6a816afb
Taja992 <Tharaxui@gmail.com>
2024-08-27 12:52:43 +0200

    Functional CRUD

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -33,20 +33,36 @@ public class PizzaController : ControllerBase
     [HttpPost]
     public IActionResult Create(Pizza pizza)
     {
-        return null;
+        PizzaServices.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
     }
     
     // PUT action
     [HttpPut("{id}")]
     public IActionResult Update(int id, Pizza pizza)
     {
-        return null;
+        if (id != pizza.Id)
+            return BadRequest();
+        var existingPizza = PizzaServices.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+        
+        PizzaServices.Update(pizza);
+        
+        return NoContent();
     }
 
     // DELETE action
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
-        return null;
+        var pizza = PizzaServices.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+        
+        PizzaServices.Delete(id);
+
+        return NoContent();
     }
 }
